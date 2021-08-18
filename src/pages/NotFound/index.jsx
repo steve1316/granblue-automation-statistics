ae71ad477701fc2d7bbefc45ca2f0f22022bfb44
Steve Tu <stevetu11@yahoo.com>
2021-08-17 17:10:02 -0700

    Fleshed out the 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         background: "#000",
+        overflow: "hidden",
+        height: "100%",
+    },
+    container: {
+        color: "#fff",
+
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+    },
+    title: {
+        fontSize: "40px",
+    },
+    subtitle: {
+        fontSize: "16px",
+    },
+    button: {
+        borderRadius: "50px",
+        background: "#01bf71",
+        color: "#000",
+        padding: "16px 32px",
+        fontSize: "16px",
+        fontWeight: "normal",
+        margin: "16px 50px",
+
+        [theme.breakpoints.down("sm")]: {
+            padding: "16px 24px",
+            fontSize: "12px",
+            margin: "0 25px",
+        },
+    },
+    link: {
+        textDecoration: "none",
+
+        "&:hover": {
+            color: "#fff",
+        },
     },
 }));
 
@@ -18,9 +58,15 @@ const NotFound = () => {
 
     return (
         <section className={classes.root}>
-            <h2>Sorry</h2>
-            <p>404 - Page cannot be found</p>
-            <Link to="/">Head back to the Home page...</Link>
+            <div className={classes.container}>
+                <h2 className={classes.title}>Sorry</h2>
+                <p className={classes.subtitle}>404 - Page cannot be found</p>
+                <Button color="primary" variant="contained" className={classes.button}>
+                    <Link to="/" className={classes.link}>
+                        Head back to the Home page...
+                    </Link>
+                </Button>
+            </div>
         </section>
     );
 };
